29562327f104b9fc6ec16327e8025a38d20b6e2b
NedelchoPenev <silenci0@abv.bg>
2018-05-28 15:39:55 +0300

    test

---
+++ ExpressJS_Fundamentals/00_Shop_Stop_Lab/index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const port = 8000
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    })
+
+    res.write('Hello!')
+    res.end()
+}).listen(port)
+
+
