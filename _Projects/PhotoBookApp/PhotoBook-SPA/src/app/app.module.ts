8c484c4e80ab78d4494d5f43361fc67165682d83
NedelchoPenev <silenci0@abv.bg>
2018-08-06 11:52:07 +0300

    spa

--- _Projects/PhotoBookApp/PhotoBook-SPA/src/app/app.module.ts
+++ _Projects/PhotoBookApp/PhotoBook-SPA/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      NavComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
-   providers: [],
+   providers: [AuthService],
    bootstrap: [
       AppComponent
    ]
